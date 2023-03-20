8b20b2e50c12999d459dd7b995df61cfc3b2c5bf
Mula <119817972+MulaTheRadish@users.noreply.github.com>
2023-03-21 09:14:05 +1100

    Update spaceship.hpp

--- spaceship.hpp
+++ spaceship.hpp
@@ -1,67 +1,80 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <cmath>
+
+#define PI 3.14159265359
 
 class Spaceship {
     public:
-        void loadTexture(int x, int y) {
-            if (!playerTexture.loadFromFile("spaceship.png")) {
-                std::cout << "Failed to load spaceship.png" << std::endl;
-                return;
+        sf::Sprite sprite;
+        sf::Texture texture;
+        sf::Vector2f direction;
+
+        void loadTexture(std::string fileName, float x, float y) {
+            if (!texture.loadFromFile(fileName)) {
+                std::cout << "Error loading " << fileName << std::endl;;
             }
-            playerSprite.setTexture(playerTexture);
-
-            up = false;
-            down = false;
-            right = false;
-            left = false;
-
-            playerSprite.setPosition(sf::Vector2f(x, y));
-            playerSprite.scale(sf::Vector2f(0.2, 0.2));
+            sprite.setTexture(texture);
+            sprite.setScale(sf::Vector2f(0.2, 0.2));
+            sprite.setOrigin(sprite.getLocalBounds().width / 2.f, sprite.getLocalBounds().height / 2.f);
+            sprite.setPosition(x, y);
         }
-        void drawto(sf::RenderWindow &window) {
-            window.draw(playerSprite);
-        }
-        void eventKey(sf::Keyboard::Key key, bool checkPressed) {
+        void userInput(sf::Keyboard::Key key, bool checkPressed) {
             if (checkPressed == true) {
-                if (key == sf::Keyboard::Key::W) {
+                if (key == sf::Keyboard::Up || key == sf::Keyboard::W) {
                     up = true;
                 }
-                else if (key == sf::Keyboard::Key::S) {
+                if (key == sf::Keyboard::Down || key == sf::Keyboard::S) {
                     down = true;
                 }
-                else if (key == sf::Keyboard::Key::A) {
-                    right = true;
-                }
-                else if (key == sf::Keyboard::Key::D) {
+                if (key == sf::Keyboard::Left || key == sf::Keyboard::A) {
                     left = true;
+                }
+                if (key == sf::Keyboard::Right || key == sf::Keyboard::D) {
+                    right = true;
                 }
             }
             else {
-                up = false;
-                down = false;
-                right = false;
-                left = false;
+                if (key == sf::Keyboard::Up || key == sf::Keyboard::W) {
+                    up = false;
+                }
+                if (key == sf::Keyboard::Down || key == sf::Keyboard::S) {
+                    down = false;
+                }
+                if (key == sf::Keyboard::Left || key == sf::Keyboard::A) {
+                    left = false;
+                }
+                if (key == sf::Keyboard::Right || key == sf::Keyboard::D) {
+                    right = false;
+                }
             }
         }
-        void update() {
-            sf::Vector2f movement;
-            if (up) {
-                movement.y += -2.0f;
+        void movement() {
+            direction.x = terminalVelocity * sin(DegToRad(sprite.getRotation()));
+            direction.y = terminalVelocity * -cos(DegToRad(sprite.getRotation()));
+
+            if (up == true) {
+                sprite.move(direction.x, direction.y);
             }
-            if (down) {
-                movement.y += 2.0f;
+            if (down == true) {
+                sprite.move(-direction.x, -direction.y);
             }
-            if (right) {
-                movement.x += -2.0f;
+            if (right == true) {
+                sprite.rotate(terminalRotation);
             }
-            if (left) {
-                movement.x += 2.0f;
+            if (left == true) {
+                sprite.rotate(-terminalRotation);
             }
-            playerSprite.move(movement);
+        }
+        void draw(sf::RenderWindow &window) {
+            window.draw(sprite);
         }
     private:
-        sf::Texture playerTexture;
-        sf::Sprite playerSprite;
-
-        bool up, down, right, left;
+        float terminalVelocity = 2.f;
+        float terminalRotation = 2.f;
+        bool up, down, left, right;
+    
+        float DegToRad(float degree) {
+            return degree * (PI / 180);
+        }
 };
